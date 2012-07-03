9e7b85929b605f79385083fff85dd92d344aac63
Robert Knight <robert.knight@mendeley.com>
2012-07-03 18:24:00 +0100

    Fix QApplication::queryKeyboardModifiers() returning incorrect values when the application is in the background.
    GetCurrentEventKeyModifiers() returns the modifiers from the last event which passed
    through the current application, this doesn't get updated if the current application is not active.
    
    When dragging an event from another (active) application over a Qt-based (inactive) application, this resulted
    in the drag action not being updated if the user changed pressed keys whilst the mouse was over the Qt window.
    
    This uses GetCurrentKeyModifiers() instead, which returns the actual state of the keyboard - better matching
    the documentation for QApplication::queryKeyboardModifiers()
    
    QTBUG-26413
    MD-18602

--- src/gui/kernel/qapplication_mac.mm
+++ src/gui/kernel/qapplication_mac.mm
@@ -1435,7 +1435,7 @@ void QApplication::restoreOverrideCursor()
 
 Qt::KeyboardModifiers QApplication::queryKeyboardModifiers()
 {
-    return qt_mac_get_modifiers(GetCurrentEventKeyModifiers());
+    return qt_mac_get_modifiers(GetCurrentKeyModifiers());
 }
 
 QWidget *QApplication::topLevelAt(const QPoint &p)
