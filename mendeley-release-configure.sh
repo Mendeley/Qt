40c61ee1aa664f66e64dddd9f4553b7c0c0a6f3b
Carles Pina <carles.pina@mendeley.com>
2012-12-19 11:05:30 +0000

    Adds an option to specify which SDK Qt should use. Applies on Mac.

--- mendeley-release-configure.sh
+++ mendeley-release-configure.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ "$#" -gt 0 ]; then
-  ./configure -nomake examples -nomake demos -no-qt3support -arch x86_64 -arch x86 -prefix /usr/local/Qt/4.8.1 -confirm-license "$@"
+  ./configure -nomake examples -nomake demos -no-qt3support -arch x86_64 -arch x86 -prefix /usr/local/Qt/4.8.1 -sdk /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.6.sdk -confirm-license "$@"
 else
-  ./configure -nomake examples -nomake demos -no-qt3support -arch x86_64 -arch x86 -prefix /usr/local/Qt/4.8.1 -confirm-license
+  ./configure -nomake examples -nomake demos -no-qt3support -arch x86_64 -arch x86 -prefix /usr/local/Qt/4.8.1 -sdk /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.6.sdk -confirm-license
 fi
