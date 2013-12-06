b2efa53f981423e90f7ae468a0e5337fdb861bb9
Robert Knight <robert.knight@mendeley.com>
2013-12-06 11:07:14 +0000

    Update mendeley-release-configure script to support building on OS X 10.9
     * Avoid specifying the SDK explicitly - this does not appear to be
       necessary any more.
     * Separate out the groups of args into vars to avoid duplication

--- mendeley-release-configure.sh
+++ mendeley-release-configure.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
+
+QT_VERSION=4.8.6
+#SDK_ARG=-sdk /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.6.sdk 
+
+if [ -z "$ARCH_ARGS" ]
+then
+	ARCH_ARGS="-arch x86_64 -arch x86"
+fi
+
+if [ -z "$BUILD_TYPE_ARGS" ]
+then
+	BUILD_TYPE_ARGS="-debug-and-release"
+fi
+
+LICENSE_ARGS="-opensource -confirm-license"
+COMPONENT_ARGS="-nomake examples -nomake demos -no-qt3support"
+CONFIGURE_ARGS="$BUILD_TYPE_ARGS $COMPONENT_ARGS $ARCH_ARGS -prefix /usr/local/Qt/$QT_VERSION $SDK_ARG $LICENSE_ARGS"
+
 if [ "$#" -gt 0 ]; then
-  ./configure -nomake examples -nomake demos -no-qt3support -arch x86_64 -arch x86 -prefix /usr/local/Qt/4.8.1 -sdk /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.6.sdk -confirm-license "$@"
+  ./configure $CONFIGURE_ARGS "$@"
 else
-  ./configure -nomake examples -nomake demos -no-qt3support -arch x86_64 -arch x86 -prefix /usr/local/Qt/4.8.1 -sdk /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.6.sdk -confirm-license
+  ./configure $CONFIGURE_ARGS
 fi
